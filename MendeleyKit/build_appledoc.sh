35615da06af5cf82659ba087b732e42c98758edd
Peter Schmidt <peter.schmidt@mendeley.com>
2014-10-02 13:26:11 +0100

    shell script to build the appledocs

---
+++ MendeleyKit/build_appledoc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# remove previous generated documentation
+if [ -d MendeleyKitHelp ]; then
+  rm -R MendeleyKitHelp
+fi
+
+# create directory
+mkdir MendeleyKitHelp
+
+# Build documentation if appledoc is installed
+if type -p appledoc &>/dev/null; then
+    appledoc --project-name MendeleyKit --project-company "Mendeley" --company-id com.mendeley --output MendeleyKitHelp --keep-intermediate-files --exit-threshold 2 --ignore .m --ignore MendeleyKitTests . 
+else
+    echo "appledoc executable can not be found, you can find installation instuctions at https://github.com/tomaz/appledoc"
+fi
