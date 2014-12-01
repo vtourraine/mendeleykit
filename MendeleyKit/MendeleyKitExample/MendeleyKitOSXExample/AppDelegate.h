f762c872588f889479283c59858a375d61aa3c82
vtourraine <me@vtourraine.net>
2014-12-01 15:24:34 +0100

    Add OS X example application

---
+++ MendeleyKit/MendeleyKitExample/MendeleyKitOSXExample/AppDelegate.h
@@ -0,0 +1,39 @@
+/*
+ ******************************************************************************
+ * Copyright (C) 2014-2017 Elsevier/Mendeley.
+ *
+ * This file is part of the Mendeley OS X SDK.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *****************************************************************************
+ */
+
+#import <Cocoa/Cocoa.h>
+
+/**
+ Note: the defines below must NEVER be checked into the mendeley github
+ repository.
+ Any pull requests containing any real values for the defines below will be rejected
+ and/or removed
+ */
+#define kMyClientSecret      @"<YOUR CLIENT SECRET>"
+#define kMyClientID          @"<YOUR CLIENT ID>"
+#define kMyClientRedirectURI @"<YOUR REDIRECT URI>"
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (nonatomic, strong) NSArray *documents;
+
+- (IBAction)signOut:(id)sender;
+
+@end
