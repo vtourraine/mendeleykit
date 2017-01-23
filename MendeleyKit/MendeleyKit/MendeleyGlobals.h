5be84d2f476cc0d0669bbaee39ab6e44d59f62f4
Luigi Trevisi <luigi.trevisi@mendeley.com>
2017-01-23 15:06:43 +0000

    bug-fixing

--- MendeleyKit/MendeleyKit/MendeleyGlobals.h
+++ MendeleyKit/MendeleyKit/MendeleyGlobals.h
@@ -481,7 +481,7 @@ typedef void (^ __nullable MendeleyStringArrayCompletionBlock)(NSArray * __nulla
 #define kMendeleyJSONDocuments                     @"documents"
 #define kMendeleyJSONGroup                         @"group"
 #define kMendeleyJSONRecommendations               @"recommendations"
-#define kMendeleyJSONUserDocument                  @"user-document"
+#define kMendeleyJSONUserDocument                  @"user_document"
 
 /***********************************************
    @name Paging
