1bdb8baad02024f87c9a75a5736f08171dd767fc
Aaron Tunney <a.tunney@elsevier.com>
2017-07-12 14:30:25 +0100

    Tagged users is actually an array of NSString objects

--- MendeleyKit/MendeleyKit/Model/MendeleyObjectHelper.m
+++ MendeleyKit/MendeleyKit/Model/MendeleyObjectHelper.m
@@ -595,14 +595,6 @@
         }
     }
     
-    if ([modelName isEqualToString:NSStringFromClass([MendeleyCommentUpdate class])])
-    {
-        if ([propertyName isEqualToString:kMendeleyJSONTaggedUsers])
-        {
-            return YES;
-        }
-    }
-    
     return NO;
 }
 
@@ -1336,58 +1328,6 @@
 
                  }];
                 return positionDicts;
-            }
-        }
-    }
-    
-    NSString *commentUpdate = NSStringFromClass([MendeleyCommentUpdate class]);
-    if ([modelName isEqualToString:commentUpdate])
-    {
-        if ([propertyName isEqualToString:kMendeleyJSONTaggedUsers])
-        {
-            if ([customObject isKindOfClass:[NSArray class]])
-            {
-                NSArray *taggedUsers = (NSArray *)customObject;
-                NSMutableArray *taggedUsersDicts = [NSMutableArray new];
-                
-                for (MendeleySocialProfile *profile in taggedUsers)
-                {
-                    NSMutableDictionary *dictionary = [NSMutableDictionary dictionary];
-                    NSDictionary *modelAttributes = [MendeleyObjectHelper propertiesAndAttributesForModel:profile];
-                    [modelAttributes enumerateKeysAndObjectsUsingBlock:^(NSString *key, NSString *attribute, BOOL *stop) {
-                        if ([attribute rangeOfString:@"NSString"].location != NSNotFound ||
-                            [attribute rangeOfString:@"NSNumber"].location != NSNotFound)
-                        {
-                            id value = [customObject valueForKey:key];
-                            if (nil != value)
-                            {
-                                [dictionary setObject:value forKey:key];
-                            }
-                        }
-                    }];
-                    [taggedUsersDicts addObject:dictionary];
-                }
-                
-                return taggedUsersDicts;
-            }
-        }
-    }
-    
-    NSString *socialProfile = NSStringFromClass([MendeleySocialProfile class]);
-    if ([modelName isEqualToString:socialProfile])
-    {
-        if ([propertyName isEqualToString:kMendeleyJSONPhotos])
-        {
-            if ([customObject isKindOfClass:[NSArray class]])
-            {
-                NSArray *photos = (NSArray *)customObject;
-                NSMutableArray *photoDicts = [NSMutableArray new];
-                for (MendeleySocialProfilePhoto *photo in photos)
-                {
-                    [photoDicts addObject:([MendeleyObjectHelper propertiesAndAttributesForModel:photo])];
-                }
-                
-                return photoDicts;
             }
         }
     }
